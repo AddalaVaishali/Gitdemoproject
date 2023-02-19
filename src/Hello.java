1fa419cc612d9f79e30e7a952ba1fffd601b27e1
AddalaVaishali <125791269+AddalaVaishali@users.noreply.github.com>
2023-02-19 19:07:07 +0530

    Updated print statements

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String Name ="simba";
+		System.out.println("Hi Everyone");
 		System.out.println(Name);
 	}
 
